47c29a8dcbc0a69d700fecd9e48fed40d9629884
Eduardo Trujillo <ed@chromabits.com>
2015-02-18 16:51:52 -0500

    Begin fetching things over Conduit

--- feedProxy.js
+++ feedProxy.js
@@ -3,9 +3,42 @@ var express = require('express'),
     config = require('config'),
     concat = require('concat-stream'),
     Qs = require('qs');
+    Canduit = require('canduit'),
     superagent = require('superagent');
 
-var app = express();
+var app = express(),
+    conduit,
+    fetchPhidData;
+
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
+
+conduit = Canduit(config.get('conduit'), function (err) {
+    if (err) {
+        throw err;
+    }
+
+    console.log('Successfully connected to Phabricator over Conduit');
+});
+
+fetchPhidData = function (phid, callback) {
+    conduit.exec(
+        'phid.query',
+        {
+            "phids": {
+                "0": phid
+            }
+        },
+        function (err, response) {
+            if (err) {
+                callback(err);
+            }
+
+            var keys = Object.keys(response);
+
+            callback(null, response[keys[0]]);
+        }
+    )
+};
 
 //app.use(bodyParser.urlencoded({ extended: true }));
 //app.use(bodyParser.json());
@@ -24,15 +57,19 @@ app.use(function(req, res, next) {
 app.post('/', function(req, res){
     res.send('OK');
 
-    superagent
-        .post(config.get('slack.url'))
-        .send({
-            "username": config.get('slack.username'),
-            "text": req.body.storyText
-        })
-        .end(function(error, res){
+    if (req.body.storyData && req.body.storyData.objectPHID) {
+        fetchPhidData(req.body.storyData.objectPHID, function (err, data) {
+            superagent
+                .post(config.get('slack.url'))
+                .send({
+                    "username": config.get('slack.username'),
+                    "text": req.body.storyText + " <" + data.uri + "More info>"
+                })
+                .end(function(error, res){
 
+                });
         });
+    }
 
     console.log(req.raw.toString('utf-8'));
 });
