8dbf3adb11ef679fd4a6d6dd549ad776bd11afe4
Eduardo Trujillo <ed@chromabits.com>
2016-09-12 00:46:43 -0700

    fix(harbormaster): Update paths

--- resources/harbormaster/build.sh
+++ resources/harbormaster/build.sh
@@ -2,9 +2,9 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-mkdir -p /home/etcinit/go/src/github.com/etcinit
+mkdir -p /home/drydock/go/src/github.com/etcinit
 ln -s $(pwd) /home/drydock/go/src/github.com/etcinit/
-cd /home/etcinit/go/src/github.com/etcinit/phabulous
+cd /home/drydock/go/src/github.com/etcinit/phabulous
 
 go get ./...
 go build cmd/phabulous/phabulous.go
