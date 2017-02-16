a30f8c7547100324aaeaacc6f7322d14763a591c
Eduardo Trujillo <ed@chromabits.com>
2017-02-16 00:24:57 -0800

    feat(core): Add support for projects back and make batch API calls

--- app/modules/core/command_summon.go
+++ app/modules/core/command_summon.go
@@ -5,6 +5,8 @@ import (
 	"strconv"
 	"strings"
 
+	"github.com/etcinit/gonduit"
+	"github.com/etcinit/gonduit/entities"
 	"github.com/etcinit/gonduit/requests"
 	"github.com/etcinit/phabulous/app/gonduit/extensions"
 	phabulousRequests "github.com/etcinit/phabulous/app/gonduit/extensions/requests"
@@ -86,7 +88,9 @@ func (c *SummonCommand) GetHandler() interfaces.Handler {
 			return
 		}
 
-		if len((*res)[0].Reviewers) == 0 {
+		revision := (*res)[0]
+
+		if len(revision.Reviewers) == 0 {
 			s.Post(
 				m.GetChannel(),
 				"Revision has no reviewers.",
@@ -97,62 +101,10 @@ func (c *SummonCommand) GetHandler() interfaces.Handler {
 			return
 		}
 
-		var slackMap *responses.PhabulousToSlackResponse
-		var slackUsers []slack.User
-
-		if sb, ok := s.(interfaces.SlackBot); ok {
-			slackMap, err = extensions.PhabulousToSlack(
-				conn,
-				phabulousRequests.PhabulousToSlackRequest{
-					UserPHIDs: (*res)[0].Reviewers,
-				},
-			)
-			if err != nil {
-				s.Excuse(m, err)
-				return
-			}
-
-			slackUsers, err = sb.GetSlack().GetUsers()
-			if err != nil {
-				s.Excuse(m, err)
-				return
-			}
-		}
-
-		reviewerNames := []string{}
-
-		for _, reviewerPHID := range (*res)[0].Reviewers {
-			if _, ok := s.(interfaces.SlackBot); ok {
-				if slackUserInfo, ok := (*slackMap)[reviewerPHID]; ok {
-					var foundUser *slack.User
-					for _, user := range slackUsers {
-						if user.ID == slackUserInfo.AccountID {
-							foundUser = &user
-							break
-						}
-					}
-
-					if foundUser != nil {
-						reviewerNames = append(
-							reviewerNames,
-							fmt.Sprintf(
-								"@%s :slack:",
-								foundUser.Name,
-							),
-						)
-
-						continue
-					}
-				}
-			}
-
-			nameRes, err := conn.PHIDQuerySingle(reviewerPHID)
-			if err != nil {
-				s.Excuse(m, err)
-				return
-			}
-
-			reviewerNames = append(reviewerNames, "@"+(*nameRes).Name)
+		reviewerNames, err := c.getReviewerNames(s, conn, revision)
+		if err != nil {
+			s.Excuse(m, err)
+			return
 		}
 
 		userName, err := s.GetUsername(m.GetUserId())
@@ -168,12 +120,216 @@ func (c *SummonCommand) GetHandler() interfaces.Handler {
 				userName,
 				strings.Join(reviewerNames, ", "),
 				matches[1],
-				(*res)[0].Title,
-				(*res)[0].StatusName,
-				(*res)[0].URI,
+				revision.Title,
+				revision.StatusName,
+				revision.URI,
 			),
 			messages.IconDefault,
 			true,
 		)
 	}
+}
+
+// getReviewerNames does a lot of magic to give us a pretty list of reviewers
+// that we can return in a message. Phbricator usernames will be used, projects
+// will be expanded, and Slack usernames will be resolved if possible.
+func (c *SummonCommand) getReviewerNames(
+	bot interfaces.Bot,
+	conn *gonduit.Conn,
+	revision *entities.DifferentialRevision,
+) ([]string, error) {
+	slackMap, slackUsers, err := c.lookupSlackMap(bot, conn, revision)
+
+	if err != nil {
+		return nil, err
+	}
+
+	reviewerNames := []string{}
+	reviewerMap, err := c.getReviewerPHIDs(conn, revision)
+
+	if err != nil {
+		return nil, err
+	}
+
+	for reviewerPHID, reviewerName := range reviewerMap {
+		// Prevent the author from embarassing themselves.
+		if revision.AuthorPHID == reviewerPHID {
+			continue
+		}
+
+		// Attempt to match on Slack.
+		if m, ok := c.findOnSlack(bot, slackMap, &slackUsers, reviewerPHID); ok {
+			reviewerNames = append(reviewerNames, m)
+		}
+
+		// Otherwise, use their Phabricator username.
+		reviewerNames = append(reviewerNames, "@"+reviewerName)
+	}
+
+	return reviewerNames, nil
+}
+
+// getReviewerPHIDs gets a mapping of PHIDs and usernames. Projects are expanded
+// if needed and configured.
+//
+// We do some batch requests, which save time and resources, but they might be
+// limited by pagination. If the diff has many attached users and projects,
+// the bot might produce partial results.
+//
+// The current implementation assumes diffs won't have a ridiculous amount of
+// reviewers attached to them, and that if projects are used, they have a
+// moderate amount of users as well.
+func (c *SummonCommand) getReviewerPHIDs(
+	conn *gonduit.Conn,
+	revision *entities.DifferentialRevision,
+) (map[string]string, error) {
+	reviewerMap := map[string]string{}
+	projects := []string{}
+
+	// We query all PHIDs in batch to avoid spamming the Phabricator server with
+	// individual requests.
+	allRes, err := conn.PHIDQuery(requests.PHIDQueryRequest{
+		PHIDs: revision.Reviewers,
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	for reviewerPHID, queryResult := range allRes {
+		// If the PHID is a project, we will keep it for later.
+		if queryResult.Type == "PROJ" {
+			projects = append(projects, reviewerPHID)
+
+			continue
+		}
+
+		reviewerMap[reviewerPHID] = queryResult.Name
+	}
+
+	// If any of the reviewers was a project, we will do some additional
+	// processing.
+	if len(projects) > 0 {
+		projectMembers := map[string]bool{}
+		projectMembersList := []string{}
+
+		// Batch request all projects.
+		projRes, err := conn.ProjectQuery(requests.ProjectQueryRequest{
+			PHIDs: projects,
+		})
+
+		if err != nil {
+			return nil, err
+		}
+
+		// Obtain a Set union of the members of all projects.
+		for _, project := range projRes.Data {
+			for _, memberPHID := range project.Members {
+				projectMembers[memberPHID] = true
+			}
+		}
+
+		// Extract all Set keys (PHIDs) into a list.
+		for projectMember := range projectMembers {
+			// Small optimization: If the member is already on the global user list,
+			// we don't include them in the member list to avoid redundant lookups.
+			if _, ok := reviewerMap[projectMember]; ok {
+				continue
+			}
+
+			projectMembersList = append(projectMembersList, projectMember)
+		}
+
+		allMembersRes, err := conn.PHIDQuery(requests.PHIDQueryRequest{
+			PHIDs: projectMembersList,
+		})
+
+		if err != nil {
+			return nil, err
+		}
+
+		// We do the same as for users above, but we assume at this point that all
+		// of them are users, not projects.
+		for reviewerPHID, queryResult := range allMembersRes {
+			reviewerMap[reviewerPHID] = queryResult.Name
+		}
+	}
+
+	return reviewerMap, nil
+}
+
+// lookupSlackMap uses the Phabulous Phabricator extension to lookup the Slack
+// account IDs of the reviewers for a revision using their PHIDs. If the
+// message is being handled by a non-Slack bot, empty results are returned.
+func (c *SummonCommand) lookupSlackMap(
+	bot interfaces.Bot,
+	conn *gonduit.Conn,
+	revision *entities.DifferentialRevision,
+) (*responses.PhabulousToSlackResponse, []slack.User, error) {
+	var slackMap *responses.PhabulousToSlackResponse
+	var slackUsers []slack.User
+	var err error
+
+	// First, we check that the bot implementation is a Slack bot.
+	if sb, ok := bot.(interfaces.SlackBot); ok {
+		// Lookup the reviewers using the extension.
+		slackMap, err = extensions.PhabulousToSlack(
+			conn,
+			phabulousRequests.PhabulousToSlackRequest{
+				UserPHIDs: revision.Reviewers,
+			},
+		)
+
+		if err != nil {
+			return nil, nil, err
+		}
+
+		// Get all Slack users from the Slack API.
+		slackUsers, err = sb.GetSlack().GetUsers()
+
+		if err != nil {
+			return nil, nil, err
+		}
+	}
+
+	return slackMap, slackUsers, nil
+}
+
+// findOnSlack attempts to find a match for reviewerPHID in slackMap and
+// slackUsers. If a match is found, it is returned along true. Otherwise, an
+// empty string and false are returned.
+func (c *SummonCommand) findOnSlack(
+	bot interfaces.Bot,
+	slackMap *responses.PhabulousToSlackResponse,
+	slackUsers *[]slack.User,
+	reviewerPHID string,
+) (string, bool) {
+	// First, we check that the bot implementation is a Slack bot.
+	if _, ok := bot.(interfaces.SlackBot); ok {
+		// Next, we check that the reviewer's PHID lookup came back with a result
+		// matching it to some Slack ID.
+		if slackUserInfo, ok := (*slackMap)[reviewerPHID]; ok {
+			var foundUser *slack.User
+
+			// We will go over the list of Slack users and attempt to find a matching
+			// account.
+			for _, user := range *slackUsers {
+				if user.ID == slackUserInfo.AccountID {
+					foundUser = &user
+
+					break
+				}
+			}
+
+			// If we found a user, we return a formatted version of their username to
+			// be added to a list of usernames.
+			if foundUser != nil {
+				formattedName := fmt.Sprintf("@%s :slack:", foundUser.Name)
+
+				return formattedName, true
+			}
+		}
+	}
+
+	return "", false
 }
